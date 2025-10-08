4c69ba156a76c8f1d79e7b0d6fe593c16736274c
Olivia Perry <153868993+4orks@users.noreply.github.com>
2025-10-08 16:08:06 +0200

    Build an astro island to present greeting in index
    This commit adds an Astro island to the index page which includes a
    button that presents a random greeting when clicked.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), preact()],
   site: "https://timely-caramel-b7c19c.netlify.app"
 });
